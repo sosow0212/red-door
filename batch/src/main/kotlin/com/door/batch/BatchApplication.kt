7e1a158e59d770111d7e9aa0c99b32dc109ef296
sosow0212 <sosow0212@naver.com>
2025-08-04 21:29:44 +0900

    feat: batch 모듈 구성

--- batch/src/main/kotlin/com/door/batch/BatchApplication.kt
+++ batch/src/main/kotlin/com/door/batch/BatchApplication.kt
@@ -2,7 +2,9 @@ package com.door.batch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
+@EnableTransactionManagement
 @SpringBootApplication
 class BatchApplication
 
