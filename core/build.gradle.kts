4041eb64b8176aef2338a3279a47077768d09c4f
sosow0212 <sosow0212@naver.com>
2025-08-08 18:50:08 +0900

    chore: netty resolver 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 
     // WebClient
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.netty:netty-resolver-dns-native-macos:4.1.123.Final:osx-aarch_64")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
 }
